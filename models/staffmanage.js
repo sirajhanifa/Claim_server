9350a73558cda4170f718e94889dd00b95f05379
sirajhanifa <sirajhanifa786@gmail.com>
2026-01-22 12:32:41 +0530

    Make staff_id required in staff schema for data integrity

--- models/staffmanage.js
+++ models/staffmanage.js
@@ -1,7 +1,7 @@
 const mongoose= require('mongoose')
 
 const staffSchema = new mongoose.Schema({
-    staff_id:{type:String,required:false},
+    staff_id:{type:String,required:true},
     staff_name:{type:String,required:true},
     department:{type:String,required:true},
     designation:{type:String,required:false},
