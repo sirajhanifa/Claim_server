82ef26fb382cf61011430bbe16c6213f910e5951
MohamedJainulHaneef <jainulhaneef03@gmail.com>
2026-04-13 16:07:25 +0530

    Refactor index.js for improved readability and organization

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
-const mongoose = require("mongoose")
-
-
-
-// Load environment variables
-require('dotenv').config({quiet: true});
-
-// Initialize Express app
 const app = express();
 
-// Enable CORS for all origins
+// -----------------------------------------------------------------------------------------------------------------
+
+require('dotenv').config({ quiet: true });
+
 app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
+// -----------------------------------------------------------------------------------------------------------------
 
-//Import Models
+// Models
+
 require('./models/user')
 require('./models/claimtype')
 require('./models/staffmanage')
 require('./models/claimEntry')
 
+// -----------------------------------------------------------------------------------------------------------------
 
+// mongoose.connection.once('open', async () => {
+//   const Staff = require('./models/staffmanage');
+//   await Staff.deleteMany({});
+//   console.log("Staff records cleared");
+// });
 
-//Routes
+// -----------------------------------------------------------------------------------------------------------------
+
+// Routes
+
 const login = require('./routes/userRoutes')
 const staffmanage = require('./routes/staffRoutes')
 const claimmanage = require('./routes/claimManageRoute')
@@ -36,8 +42,10 @@ const cliamReport = require('./routes/claimReport')
 const Setting = require('./routes/settingRoute')
 const Dashboard = require('./routes/dashboardRoute')
 const PaymentProcess = require('./routes/paymentProcess')
-const adminPaymentStatusRoutes  = require("./routes/adminPaymentStatus")
+const adminPaymentStatusRoutes = require("./routes/adminPaymentStatus")
 const adminMaintenanceRoutes = require("./routes/adminMaintenanceRoutes")
+
+// -----------------------------------------------------------------------------------------------------------------
 
 // Connect to MongoDB
 connectDB();
@@ -45,17 +53,10 @@ connectDB();
 // Middleware
 app.use(express.json());
 
-// mongoose.connection.once('open', async () => {
-//   const Staff = require('./models/staffmanage');
-//   await Staff.deleteMany({});
-//   console.log("✅ Staff records cleared");
-// });
+// -----------------------------------------------------------------------------------------------------------------
 
+// Routes
 
-
-
-
-// Set the routing
 app.use('/api', login)
 app.use('/api', claimmanage)
 app.use('/api/staff', staffmanage)
@@ -67,11 +68,14 @@ app.use('/api/finance', PaymentProcess)
 app.use('/api/admin/payment-status', adminPaymentStatusRoutes);
 app.use("/api/admin/maintenance", adminMaintenanceRoutes);
 
-
-
+// -----------------------------------------------------------------------------------------------------------------
 
 // Start server
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 5001;
+
 app.listen(PORT, () => {
-  console.log(`✅ Server running at http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
+
+// -----------------------------------------------------------------------------------------------------------------
