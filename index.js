257031d041ff5e9df8bc18ff58db9f49a2c09b36
MohamedJainulHaneef <jainulhaneef03@gmail.com>
2026-04-15 22:34:01 +0530

    Remove trailing whitespace in index.js for cleaner code

--- index.js
+++ index.js
@@ -77,5 +77,5 @@ const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
-
+ 
 // -----------------------------------------------------------------------------------------------------------------
